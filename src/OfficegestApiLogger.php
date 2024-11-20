831e34addfbd59adaae3c3509e7de4b7772eae9e
Stephane Soares <stephane.soares@guisoft.net>
2024-11-20 16:46:33 +0000

    Add timestamp to index name (suffix)

--- src/OfficegestApiLogger.php
+++ src/OfficegestApiLogger.php
@@ -35,7 +35,7 @@ final class OfficegestApiLogger
                 'version' => self::VERSION,
                 'sdk' => 'laravel',
                 'user' => auth()->user()->username ?? null,
-                '@timestamp' => date('Y-m-d H:i:s.v'),
+                'timestamp' => date('Y-m-d H:i:s.v'),
             ],
             [
                 'data' => $data->__toArray(),
